77b77ae459c33a333994f902b17322764ed0061f
Dobromir Hristov <dobromir92@gmail.com>
2018-06-17 21:39:58 +0300

    chore: last paragraph refactor

--- components/resume/Resume.vue
+++ components/resume/Resume.vue
@@ -76,10 +76,14 @@
         :item="item"
         type="oss"/>
       <!-- eslint-disable vue/max-attributes-per-line -->
+      <p class="leading-content-text mb-4 m-auto w-2/3">
+        Along with packages, I have written articles, contributed to documentations, even authored a whole page in the official Vue multilingual
+        library
+        about <a class="text-inherit no-underline font-bold" href="https://kazupon.github.io/vue-i18n/guide/lazy-loading.html" target="_blank">Lazy
+        loading translation files</a>.
+      </p>
       <p class="leading-content-text mb-xl m-auto w-2/3">
-        Along with packages, I have written articles, contributed to documentations, even authored a whole page in the official Vue multilingual library
-        about <a class="text-inherit no-underline font-bold" href="https://kazupon.github.io/vue-i18n/en/lazy-loading.html" target="_blank">Lazy loading translation files</a>.<br>
-        I have many open source projects and tutorials that are in the works, from advanced Vue use cases to basics that are so apparent to beginners.
+        I have many other open source projects and tutorials that are in the works, from tips targeted at beginners to advanced real world use cases.
       </p>
     </div>
   </div>
